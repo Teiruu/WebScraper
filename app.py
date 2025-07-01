4204e56d483f373c269e86f575208af4b1e9238e
teiruu <ryanloughman11@gmail.com>
2025-07-01 15:55:12 +0100

    Added db functionality

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from WebCrawler.models import Scraper
+from WebCrawler.models import Scraper, Link, Base
 from flask import Flask, render_template, redirect, url_for
 import requests
 from bs4 import BeautifulSoup
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+
+# create engine and session
+engine = create_engine('sqlite:///links.db')
+Session = sessionmaker(bind=engine)
+session = Session()
 app = Flask(__name__)
 app.config.from_pyfile('instance/config.py')
 
+# create the table
+Base.metadata.create_all(engine)
 
 def scrape_links(url):
     response = requests.get(url)
@@ -18,6 +27,13 @@ def index():
     links = None
     if form.validate_on_submit():
         links = scrape_links(form.url.data)
+        for link in links:
+            href = link.get('href')
+            if href:  # only save valid hrefs
+                if not session.query(Link).filter_by(url=href).first():
+                    new_link = Link(url=href)
+                    session.add(new_link)
+        session.commit()
     return render_template('index.html', form=form, links=links)
 
 
--- models.py
+++ models.py
@@ -1,8 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms.fields.simple import StringField
 from wtforms.validators import InputRequired
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
+# define model
+class Link(Base):
+    __tablename__ = 'links'
+    id = Column(Integer, primary_key=True)
+    url = Column(String, unique=True)
+
+# user input website desired
 class Scraper(FlaskForm):
     url = StringField('link_label',
                            validators=[InputRequired(message="Please enter a valid url")])
